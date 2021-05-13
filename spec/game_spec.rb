aec00f080c5a77c248949999b2c9e521b026a1ac
Charlie Slater <charliemslater1701@gmail.com>
2021-05-13 15:20:45 +0100

    made controller skinnier

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,20 @@
 require 'game'
 
 describe Game do
+  let(:player_1) { double("Player") }
+  let(:player_2) { double("Player") }
+  let(:subject) { described_class.new(player_1, player_2) }
+  
   describe '#attack' do
-    let(:charlotte) { Player.new("Charlotte") }
-    it 'attacks a player' do
-      # subject.attack(charlotte)
-      expect{ subject.attack(charlotte) }.to change{ charlotte.hp }.by(-10)
+    it 'attacks a second player' do
+      expect(player_2).to receive(:take_damage)
+      subject.attack(player_2)
+    end
+  end
+
+  describe '#player_one' do
+    it 'returns a instance of a player' do
+      expect(subject.player_1).to eq(player_1)
     end
   end
 end
